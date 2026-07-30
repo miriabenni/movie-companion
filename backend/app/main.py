ceb6c643c5b9485c3fd7fa2aa10d451c3b23cdcf
miriabenni <miria.benni@inapp.com>
2026-07-30 13:14:36 +0530

    changed cors url to a genera vercel url

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ app= FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://movie-companion-psi.vercel.app","http://localhost:5173"],
+    allow_origins=["https://*.vercel.app","http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
