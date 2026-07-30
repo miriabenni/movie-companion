55c5e68fb21b7a2626759cec5433e6c4cd558c34
miriabenni <miria.benni@inapp.com>
2026-07-30 13:23:39 +0530

    changed allow_credentials to false. Now cors allow all origins.

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,8 +7,8 @@ app= FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://*.vercel.app","http://localhost:5173"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
